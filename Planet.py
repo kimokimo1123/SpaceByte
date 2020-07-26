82f2cd6a8f7819f3e6a2ea231c71c3d953d982cf
kimokimo1123 <68424469+kimokimo1123@users.noreply.github.com>
2020-07-26 04:30:48 +0200

    x/y coordinates

--- Planet.py
+++ Planet.py
@@ -13,3 +13,11 @@ class Planet:
     def radius_fi(self):
         radius_n = self.kleine_ha/math.sqrt(1-(self.numerische_ex**2)*(math.cos(self.winkel_fi)**2))
         return radius_n
+
+    def x_coordin(self):
+        x_coord = self.grosse_ha*(math.cos(self.winkel_fi))
+        return x_coord
+    
+    def y_coordin(self):
+        y_coord = self.kleine_ha*(math.sin(self.winkel_fi))
+        return y_coord
--- main.py
+++ main.py
@@ -6,4 +6,6 @@ my_earth = Planet('Earth', 149597890, 149577002.3, 2499813.6534358, 0.01671022, 
 
 for angle in numpy.arange(0, 2*math.pi+math.pi/180, math.pi/180):
     my_earth.winkel_fi = angle
-    print(angle, my_earth.radius_fi())
+    print(round(my_earth.x_coordin()), round(my_earth.y_coordin()))
+
+    
